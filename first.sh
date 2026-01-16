7f2e95d520741a91264daebc5d350d3f4c07181a
Test User <test@example.com>
2026-01-16 14:46:58 +0530

    Add all files including 16jan folder

---
+++ first.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "This is a first shell script"
+echo "I am from GLA University"
+echo "Currently studying in 6th sem of btech cse"
+echo "My cpi is 6.2"
+echo "My dream is to support my father in bussnies"
